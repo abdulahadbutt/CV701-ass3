77a92a39c810ed8729f891075f772489431d87b0
Abdul Ahad <ahadbutt.pk@gmail.com>
2024-10-24 19:12:33 +0400

    initial run completed

--- src/train.py
+++ src/train.py
@@ -22,6 +22,7 @@ def train_one_epoch(
     criterion: torch.nn.CrossEntropyLoss,
     device:str='cpu'  
 ):
+    model.train()
     with tqdm(data_loader, unit='batch') as data:
         batch_loss_list = []
         for batch in data:
@@ -54,33 +55,91 @@ def train_one_epoch(
     }
 
 
+def test_one_epoch(
+    model: torch.nn.Module,
+    optimizer: torch.optim,
+    data_loader: torch.utils.data.DataLoader,
+    epoch_index: int,
+    criterion: torch.nn.CrossEntropyLoss,
+    device:str='cpu' 
+):
+    model.eval()
+    num_correct = 0
+    num_samples = 0
+    with torch.no_grad():
+        with tqdm(data_loader, unit='batch') as data:
+            batch_loss_list = []
+            for batch in data:
+                data.set_description(f"Testing after epoch{epoch_index}")
+
+                # ? Feeding to CNN
+                inputs, labels = batch[0].to(device), batch[1].to(device)
+                outputs = model(inputs)
+                
+                # ? Getting Loss
+                batch_loss = criterion(outputs, labels)
+                batch_loss_list.append(batch_loss.item())
+
+                data.set_postfix(
+                    batch_loss=batch_loss.item()
+                )
+
+                _, predictions = outputs.max(1)
+                num_correct += (predictions == labels).sum()
+                num_samples += predictions.size(0)
+
+
+    
+    return {
+        'epoch_idx': epoch_index,
+        'batch_losses': batch_loss_list,
+        'epoch_loss': np.mean(batch_loss_list),
+        'accuracy': (num_correct / num_samples).item()
+    }
+
+
 
 def train(
     model: torch.nn.Module,
     optimizer: torch.optim,
-    data_loader: torch.utils.data.DataLoader,
+    train_dataloader: torch.utils.data.DataLoader,
     epochs: int,
     criterion: torch.nn.CrossEntropyLoss,
     device:torch.device,
-    live: Live
+    live: Live,
+    test_dataloader: torch.utils.data.DataLoader
+    
 ):
 
     
-
-    model.train()
-
-    epoch_statistics_list = []
+    best_acc = 0
+    train_statistics_list = []
     for epoch in range(epochs):
-        epoch_statistics = train_one_epoch(
-            model, optimizer, data_loader, epoch, criterion, device
+        # * Training Code
+        train_epoch_statistics = train_one_epoch(
+            model, optimizer, train_dataloader, epoch, criterion, device
         )
-        
-        epoch_statistics_list.append(epoch_statistics)
-        live.log_metric('train/loss', epoch_statistics['epoch_loss'], plot=True)
+        train_statistics_list.append(train_epoch_statistics)
+        live.log_metric('train/loss', train_epoch_statistics['epoch_loss'], plot=True)
+
+        # * Testing Code
+        test_epoch_statistics = test_one_epoch(
+            model, optimizer, test_dataloader, epoch, criterion, device
+        )
+        latest_test_acc = test_epoch_statistics['accuracy']
+        if latest_test_acc > best_acc:
+            print(f'UPDATING BEST ACC [{best_acc}] -> [{latest_test_acc}]')
+            best_acc = latest_test_acc
+            save_checkpoint(model, epoch, optimizer, best_acc, 'models/best_model.pth')
+        print(latest_test_acc, type(latest_test_acc))
+        live.log_metric('test/loss', test_epoch_statistics['epoch_loss'], plot=True)
+        live.log_metric('test/accuracy', test_epoch_statistics['accuracy'], plot=True)
+
+
+
         live.next_step()
 
-
-    return epoch_statistics
+    return train_statistics_list
 
 
 def save_checkpoint(
@@ -120,6 +179,7 @@ torch.manual_seed(1)
 live = Live('metrics', dvcyaml=False, save_dvc_exp=True)
 
 os.makedirs('models', exist_ok=True)
+os.makedirs('metrics/', exist_ok=True)
 
 train_dataset = ImageWoof(
     ROOT_DIR, 
@@ -127,8 +187,18 @@ train_dataset = ImageWoof(
     train=True,
 )
 
+test_dataset = ImageWoof(
+    ROOT_DIR,
+    IMG_SIZE,
+    train=False
+)
+
 train_dataloader = torch.utils.data.DataLoader(
     train_dataset, batch_size=BATCH_SIZE, shuffle=True
+)
+
+test_dataloader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=BATCH_SIZE, shuffle=False
 )
 
 
@@ -150,7 +220,7 @@ else:
 
 criterion = torch.nn.CrossEntropyLoss()
 loss_statistics = train(
-    model, optimizer, train_dataloader, EPOCHS, criterion, device, live
+    model, optimizer, train_dataloader, EPOCHS, criterion, device, live, test_dataloader
 )
 
 
