71e47bc8118464c9071072e7655ccb91c7301688
Captain Ahab <ahadbutt.pk@gmail.com>
2024-10-27 08:51:09 +0000

    finished task 1, experiments on batchnorm,dropout
    also made some changes on conv num filters which were bad

--- src/model.py
+++ src/model.py
@@ -8,21 +8,28 @@ class CNN(nn.Module):
 
         self.feature_extractor = nn.Sequential(
             nn.Conv2d(3, 8, 3, 1),
+            nn.BatchNorm2d(num_features=8),
             nn.ReLU(),
             nn.Conv2d(8, 8, 3, 1),
+            nn.BatchNorm2d(num_features=8),
             nn.ReLU(),
             nn.MaxPool2d(2),
             #####
             nn.Conv2d(8, 16, 3, 1),
+            nn.BatchNorm2d(num_features=16),
             nn.ReLU(),
             nn.Conv2d(16, 16, 3, 1),
+            nn.BatchNorm2d(num_features=16),
             nn.ReLU(),
             nn.MaxPool2d(2),
             #####
             nn.Conv2d(16, 32, 3, 1),
+            nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
             nn.Conv2d(32, 32, 3, 1),
+            nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
+
             nn.AdaptiveAvgPool2d(
                 output_size=8,
             ),
@@ -30,10 +37,13 @@ class CNN(nn.Module):
         self.classifier = nn.Sequential(
             nn.Linear(32 * 8 * 8, 1024),
             nn.ReLU(),
+            nn.Dropout(),
             nn.Linear(1024, 512),
             nn.ReLU(),
+            nn.Dropout(),
             nn.Linear(512, 128),
             nn.ReLU(),
+            nn.Dropout(),
             nn.Linear(128, num_classes),
         )
 
